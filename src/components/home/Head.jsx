be2448ad42f8cbdcf6ec16f92cb72661187c5e77
heewon <hw7666@sogang.ac.kr>
2024-05-20 03:00:43 +0900

    💄 design: 홈페이지 컨텐츠 영역 완료

---
+++ src/components/home/Head.jsx
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+import logoIcon from '../../assets/icon/logo-white.svg';
+import { useNavigate } from 'react-router-dom';
+import leftImage from '../../assets/image/home-3.png';
+import centerImage from '../../assets/image/home-2.png';
+import rightImage from '../../assets/image/home-1.png';
+
+const NAV = [
+  { text: '캘린더', src: '/calendar' },
+  {
+    text: '지원사업 리스트',
+    src: '/list',
+  },
+  { text: 'AI 사업계획서', src: '/business' },
+];
+
+function Head() {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Header>
+        <Logo src={logoIcon} onClick={() => navigate('/')} />
+        <NavWrapper>
+          {NAV.map(({ text, src }) => (
+            <NavText key={text} onClick={() => navigate(src)}>
+              {text}
+            </NavText>
+          ))}
+        </NavWrapper>
+      </Header>
+      <MainText>
+        지원사업 확인부터 사업계획서 작성까지 모두
+        <br />
+        <Span>
+          <Bold>브릿지AI 정부지원 캘린더</Bold>와 함께
+        </Span>
+      </MainText>
+      <ImageWrapper>
+        <Image src={leftImage} />
+        <Image src={centerImage} />
+        <Image src={rightImage} />
+      </ImageWrapper>
+    </Container>
+  );
+}
+
+export default Head;
+
+const Container = styled.div`
+  height: 848px;
+  width: 1440px;
+  margin: 0 auto;
+  background-color: #3686ff;
+`;
+
+const Header = styled.div`
+  padding: 40px 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 56px;
+  height: 56px;
+
+  cursor: pointer;
+`;
+
+const NavWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const NavText = styled.p`
+  cursor: pointer;
+  width: 160px;
+
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: -0.32px;
+`;
+
+const MainText = styled.div`
+  margin-top: 24px;
+
+  color: #fff;
+  text-align: center;
+  font-size: 26px;
+  font-weight: 400;
+  letter-spacing: -1px;
+  line-height: 64px;
+`;
+
+const Span = styled.span`
+  font-size: 56px;
+`;
+
+const Bold = styled.span`
+  font-weight: 700;
+`;
+
+const ImageWrapper = styled.div`
+  padding: 72px 0;
+
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: repeat(3, 540px);
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: 540px;
+  height: 383px;
+
+  border-radius: 16px;
+  object-fit: cover;
+`;
--- src/pages/home/Layout.jsx
+++ src/pages/home/Layout.jsx
@@ -1,44 +1,23 @@
 import { useNavigate } from 'react-router-dom';
-import Logo from '../../assets/icon/logo-white.svg';
 import styled from 'styled-components';
+import Head from '../../components/home/Head';
+import Content from '../../components/home/Content';
 
 function Layout() {
   const navigate = useNavigate();
 
   return (
-    <Container onClick={() => navigate('/list')}>
-      <Image src={Logo} />
-      <Msg>
-        이동하시려면 <br />
-        화면을 클릭하세요
-      </Msg>
-    </Container>
+    <>
+      <HeadWrapper>
+        <Head />
+      </HeadWrapper>
+      <Content type="calendar" />
+      <Content type="ai" />
+    </>
   );
 }
 export default Layout;
 
-const Container = styled.div`
+const HeadWrapper = styled.div`
   background-color: #3686ff;
-  width: 100vw;
-  height: 100vh;
-  text-align: center;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 48px;
-
-  cursor: pointer;
-`;
-const Image = styled.img`
-  width: 126px;
-  height: 126px;
-`;
-const Msg = styled.p`
-  text-align: center;
-  color: #fff;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: -0.4px;
 `;
