6ba9035fd8718c6ba8941934e83766096e0ef174
heewon <hw7666@sogang.ac.kr>
2024-08-03 02:01:33 +0900

    💄 design: 홈화면 글씨 줄바꿈 깨지는 문제 수정

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <Container>
       브릿지 AI 정부지원사업캘린더
       <DetailText>
-        주식회사 에이아이라이프플래너 | 대표자 손승연 | 사업자등록번호 824-81-02970 | 문의 0507-1419-8037 | <br />
+        주식회사 에이아이라이프플래너 | 대표자 손승연 | 사업자등록번호 824-81-02970 | 문의 0507-1419-8037 |<br />
         주소 강원특별자치도 강릉시 사임당로 641-28, 102호
       </DetailText>
     </Container>
@@ -23,7 +23,7 @@ const Container = styled.div`
   background-color: #11223b;
 
   color: #f7f7f8;
-  font-size: 18px;
+  font-size: 1.8rem;
   font-weight: 500;
   line-height: 170%; /* 30.6px */
   letter-spacing: -0.18px;
@@ -32,6 +32,8 @@ const Container = styled.div`
 const DetailText = styled.p`
   margin-top: 24px;
 
-  font-size: 14px;
+  font-size: 1.4rem;
   letter-spacing: -0.14px;
+
+  white-space: nowrap;
 `;
