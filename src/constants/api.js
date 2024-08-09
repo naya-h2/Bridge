f9a2cc6fee088c94b275b834eb8550d18ffd24c7
heewon <hw7666@sogang.ac.kr>
2024-08-09 13:48:15 +0900

    ⚡️ update: pay 관련 UI & 로직 수정

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,2 @@
-export const PROXY = window.location.hostname === 'localhost' ? '' : 'https://api.bridgeai.co.kr';
+// export const PROXY = window.location.hostname === 'localhost' ? '' : 'https://api.bridgeai.co.kr';
+export const PROXY = 'https://api.bridgeai.co.kr';
