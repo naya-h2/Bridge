800f6105091725a9aafffce7e6da95aa16d4b130
heewon <hw7666@sogang.ac.kr>
2024-08-21 16:40:25 +0900

    🐛 fix: 메타태그 프로퍼티명 변경

--- src/pages/calendar/CalendarPage.jsx
+++ src/pages/calendar/CalendarPage.jsx
@@ -9,8 +9,8 @@ function CalendarPage() {
         <title>{META_TAG.calendar.title}</title>
         <meta name="title" content={META_TAG.calendar.title} />
         <meta name="description" content={META_TAG.calendar.description} />
-        <meta name="og:title" content={META_TAG.calendar.title} />
-        <meta name="og:description" content={META_TAG.calendar.description} />
+        <meta property="og:title" content={META_TAG.calendar.title} />
+        <meta property="og:description" content={META_TAG.calendar.description} />
       </Helmet>
       <Layout />
     </>
--- src/pages/list/ListPage.jsx
+++ src/pages/list/ListPage.jsx
@@ -9,8 +9,8 @@ function ListPage() {
         <title>{META_TAG.list.title}</title>
         <meta name="title" content={META_TAG.list.title} />
         <meta name="description" content={META_TAG.list.description} />
-        <meta name="og:title" content={META_TAG.list.title} />
-        <meta name="og:description" content={META_TAG.list.description} />
+        <meta property="og:title" content={META_TAG.list.title} />
+        <meta property="og:description" content={META_TAG.list.description} />
       </Helmet>
       <Layout />
     </>
