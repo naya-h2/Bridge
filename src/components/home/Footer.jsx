84c6d208b22bf77bae1ac10b3a4dca5f950ea7b2
heewon <hw7666@sogang.ac.kr>
2024-10-10 10:33:17 +0900

    💄 design: Footer 영역 회사명 변경

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <Container>
       브릿지 AI 정부지원사업캘린더
       <DetailText>
-        주식회사 에이아이라이프플래너 | 대표자 손승연 | 사업자등록번호 824-81-02970 | 문의 0507-1419-8037 |<br />
+        주식회사 웰이너스 | 대표자 손승연 | 사업자등록번호 824-81-02970 | 문의 0507-1419-8037 | 
         주소 강원특별자치도 강릉시 사임당로 641-28, 102호
       </DetailText>
     </Container>
@@ -33,6 +33,4 @@ const DetailText = styled.p`
 
   font-size: 1.4rem;
   letter-spacing: -0.14px;
-
-  white-space: nowrap;
 `;
